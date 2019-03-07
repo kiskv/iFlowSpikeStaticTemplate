de6e5e8fb8d4e2a353cc85787c75739c6f353002
Александр Яхатин <a-yahatin@Compulink.local>
2019-03-07 10:47:49 +0300

    Обновлен container: EditView

--- app/containers/EditView/index.js
+++ app/containers/EditView/index.js
@@ -38,6 +38,8 @@ export class EditView extends React.Component {
     if(this.props.selected.length === 0) {
       if(!this.props.location.state){ 
         this.props.history.push(`/grid/${this.props.match.params.gridType}`)
+      } else {
+        this.getFormElements();
       }
     } else {
       this.getFormElements();
@@ -45,51 +47,88 @@ export class EditView extends React.Component {
   }
 
   getFormElements = () => {
-    const fetchData = new Proxy('Domain.PE_RD_Debtors');
-    const loadOptions = {
-      data: [{
-        viewId: 'pe_rd_debtors_listview',
-        curViewId: 'pe_rd_debtors_listview',
-        select: 'LINK,N_Code,C_Name1,C_Name2,C_Name3,C_Address1,N_Debit1_WO_Peni,N_Debit1_Peni,N_Debit1,N_DebtPeriods1,N_OverDuePeriods1,D_Date_Due,D_Date_LastDue',
-        filter: [
-          {
-            property: 'LINK',
-            value: this.props.selected[0].LINK,
+    if(this.props.location.state) {
+      const data = {
+        "LINK": '',
+        "N_Code": '',
+        "C_Name1": "",
+        "C_Name2": "",
+        "C_Name3": "",
+        "C_Address1": "",
+        "N_Debit1_WO_Peni": 0.0,
+        "N_Debit1_Peni": 0.0,
+        "N_Debit1": 0,
+        "N_DebtPeriods1": 0.0,
+        "N_OverDuePeriods1": 0,
+        "D_Date_Due": "",
+        "D_Date_LastDue": "",
+      };
+      const formItems = [{
+        itemType: 'group',
+        colSpan: 3,
+        items: [],
+      }];
+      const keys = Object.keys(data);
+      keys.map((item) => {
+        formItems[0].items.push({
+          dataField: item,
+          label: {
+            text: ruColumns.find(el => el.name === item).text,
           },
-        ],
-        page: 1,
-        start: 0,
-        limit: 25,
-        sort: [{ property: 'C_Address1', direction: 'ASC' }],
-      }],
-    };
-    fetchData.get(loadOptions)
-      .then((data) => {
-        if (data) {
-          const { result } = data[0];
-          if (result.records.length > 0) {
-            const keys = Object.keys(result.records[0]);
-            const formItems = [{
-              itemType: 'group',
-              colSpan: 3,
-              items: [],
-            }];
-            keys.map((item) => {
-              formItems[0].items.push({
-                dataField: item,
-                label: {
-                  text: ruColumns.find(el => el.name === item).text,
-                },
-              });
-              return null;
-            });
-            this.setState({
-              formItems,
-              data: result.records[0],
-            });
-          }
-        }
+        });
+        return null;
       });
+      this.setState({
+        data,
+        formItems,
+      })
+    } else {
+      const fetchData = new Proxy('Domain.PE_RD_Debtors');
+      const loadOptions = {
+        data: [{
+          viewId: 'pe_rd_debtors_listview',
+          curViewId: 'pe_rd_debtors_listview',
+          select: 'LINK,N_Code,C_Name1,C_Name2,C_Name3,C_Address1,N_Debit1_WO_Peni,N_Debit1_Peni,N_Debit1,N_DebtPeriods1,N_OverDuePeriods1,D_Date_Due,D_Date_LastDue',
+          filter: [
+            {
+              property: 'LINK',
+              value: this.props.selected[0].LINK,
+            },
+          ],
+          page: 1,
+          start: 0,
+          limit: 25,
+          sort: [{ property: 'C_Address1', direction: 'ASC' }],
+        }],
+      };
+      fetchData.get(loadOptions)
+        .then((data) => {
+          if (data) {
+            const { result } = data[0];
+            if (result.records.length > 0) {
+              const keys = Object.keys(result.records[0]);
+              const formItems = [{
+                itemType: 'group',
+                colSpan: 3,
+                items: [],
+              }];
+              keys.map((item) => {
+                formItems[0].items.push({
+                  dataField: item,
+                  label: {
+                    text: ruColumns.find(el => el.name === item).text,
+                  },
+                });
+                return null;
+              });
+              this.setState({
+                formItems,
+                data: result.records[0],
+              });
+            }
+          }
+        });
+    }
   }
 
   render() {
@@ -99,6 +138,7 @@ export class EditView extends React.Component {
           <title>EditView</title>
           <meta name="description" content="Description of EditView" />
         </Helmet>
+        <h3>{this.props.location.state ? 'Добавление новой строки' : `Редактирование по LINK: ${this.props.selected.length > 1 ? this.props.selected[0].LINK : ''}`}</h3>
         <Form formData={this.state.data} items={this.state.formItems} scrollingEnabled />
       </Container>
     );
